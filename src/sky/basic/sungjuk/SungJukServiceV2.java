8ee52b0980bd0335e2bebedb673799e049bc3244
haneul-lim <limsky369@gamil.com>
2021-04-01 18:03:15 +0900

    성적관리프로그램 V6

---
+++ src/sky/basic/sungjuk/SungJukServiceV2.java
@@ -0,0 +1,18 @@
+package sky.basic.sungjuk;
+
+// 성적 서비스 V2 - 성적처리V6
+// 입력, 처리, 출력, 수정, 삭제 기능 구현
+// 프로그램 메뉴 출력 기능 추가
+
+public interface SungJukServiceV2 {
+
+    void displayMenu();
+    void computeSungJunk(SungJukVO sj);
+
+    void newSungJuk();
+    void showSungJuk();
+    void showOneSungJuk();
+    void modifySungJuk();
+    void removeSungJuk();
+
+}
